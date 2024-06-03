788964ae9ef6cddd008143b9087bca8ba7812ec8
Emil Yusupov <mrjava3300@mail.ru>
2024-06-03 09:39:00 +0300

    updated resources

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-    $('.slider-val').select2({
-        width: 'element',
-        tags: true,
-        placeholder: $('.slider-val').data('placeholder'),
-
+    $('.slider-val').select2({ // Changed selector to match the class in HTML
+        placeholder: $('.slider-val').data('placeholder'), // Set placeholder text
+        width:fit-content,
+        tags: true // Enable tagging/searching for options
     });
+});
+
+
+$(document).ready(function() {
+
+  $('.slider-val').select2( {
+    tags: true });
+  
 });
 
 function toggleDisplayAndSaveState() {
@@ -16,5 +23,5 @@ function toggleDisplayAndSaveState() {
 
 document.addEventListener('DOMContentLoaded', function() {
     var x = document.getElementById("display-container");
-        x.style.display = "block";
+    x.style.display = "block";
 });
