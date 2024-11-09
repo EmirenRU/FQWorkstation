9f0658f48a10f7283c138bbc0d8fd35306acc5e8
PodlesniyIvanRUDN <podlessnaja101@gmail.com>
2024-11-09 16:38:19 +0300

    js get picture data

--- hub/src/static/JS/main.js
+++ hub/src/static/JS/main.js
@@ -3,8 +3,9 @@ let appContainer = document.getElementById("application-container");
 let options = {
     method: 'GET'
 }
+let appsNumber;
 async function getAppsNumber(){
-    let appsNumber = await fetch('/hub_application/api', options)
+    appsNumber = await fetch('/hub_application/api', options)
 }
 let gridCells = 9;
 
@@ -18,16 +19,33 @@ else{
 for(let i = 0; i < gridCells; i++){
     let applicationConteainer = document.createElement("div");
     let image = document.createElement('img');
-    let imageName = "{image"+ i + ".jpg}";
-    image.setAttribute = ("th:src",imageName);
-    let applicationName = document.createElement("h2");
-    let name = "${stlec.lecturer.name"+i+"}";
-    applicationName.setAttribute("th:text", name);
+    let imageName = document.createElement('span');
+    let url;
+    let imageData = await fetch(' /api/recieve/id'+i, {
+        method: 'GET'});
+    if(imageData.ok){
+        imageName.textContent = imageData.name;
+        url = imageData.url;
+        let id = imageData.imgId;
+        let imgBlob = await fetch('/api/recieve_img/'+id, {
+            method: 'GET',
+            headers: {
+                Accept:  'image/jpeg',
+                'Content-Type': 'image/jpeg'
+            }
+        })
+        image.src = URL.createObjectURL(imgBlob);
+
+    }
+    let applicationName = document.createElement('span');
+    let name = imageName;
+    applicationName.textContent = name;
     applicationConteainer.append(image);
     applicationConteainer.append(applicationName);
     applicationConteainer.addEventListener("click", function(e){
-        let applicationLink = "http://adress.com/application"+i;
+        let applicationLink = url;
         window.open(applicationLink,'_blank');
         
     })
+    
 }
