0ab19429f312e3117ce2104cfca552bf85c15101
PodlesniyIvanRUDN <podlessnaja101@gmail.com>
2024-11-13 14:22:57 +0300

    design placeholder

--- hub/src/static/JS/main.js
+++ hub/src/static/JS/main.js
@@ -1,51 +1,71 @@
 let appContainer = document.getElementById("application-container");
 
-let options = {
-    method: 'GET'
-}
-let appsNumber;
-async function getAppsNumber(){
-    appsNumber = await fetch('/hub_application/api', options)
-}
-let gridCells = 9;
+async function main() {
 
-if(appsNumber.ok){
-    gridCells = appsNumber;
-}
-else{
-    alert("Не удалось загрузить сетку приложений");
-}
-
-for(let i = 0; i < gridCells; i++){
-    let applicationConteainer = document.createElement("div");
-    let image = document.createElement('img');
-    let imageName = document.createElement('span');
-    let url;
-    let imageData = await fetch(' /api/recieve/id'+i, {
-        method: 'GET'});
-    if(imageData.ok){
-        imageName.textContent = imageData.name;
-        url = imageData.url;
-        let id = imageData.imgId;
-        let imgBlob = await fetch('/api/recieve_img/'+id, {
-            method: 'GET',
-            headers: {
-                Accept:  'image/jpeg',
-                'Content-Type': 'image/jpeg'
-            }
-        })
-        image.src = URL.createObjectURL(imgBlob);
-
+    let options = {
+        method: 'GET'
     }
-    let applicationName = document.createElement('span');
-    let name = imageName;
-    applicationName.textContent = name;
-    applicationConteainer.append(image);
-    applicationConteainer.append(applicationName);
-    applicationConteainer.addEventListener("click", function(e){
-        let applicationLink = url;
-        window.open(applicationLink,'_blank');
-        
-    })
-    
+// async function getAppsNumber(){
+//     let response =  await fetch('/api/get-grid', options)
+//     return await response.body;
+// }
+
+    async function getGridData() {
+        let response = await fetch('/api/get-grid', options);
+        return response.json();
+    }
+
+    // let response = async () => {
+    //     let response = ;
+    //     return response
+    // };
+
+    let gridCells = await getGridData();
+// if(appsNumber.ok){
+//     gridCells = appsNumber;
+// }
+// else{
+//     alert("Не удалось загрузить сетку приложений");
+// }
+
+    for (let i = 0; i < gridCells; i++) {
+        let applicationContainer = document.createElement("div");
+        applicationContainer.classList.add('hub__application__space');
+        let image = document.createElement('img');
+        image.classList.add('hub__application__icon')
+        let imageName;
+        let url;
+        let imageData = await fetch(
+            '/api/recieve/' + i,
+            {method: 'GET'}
+        );
+        if (imageData.ok) {
+            let json = await imageData.json();
+            imageName.textContent = json.name;
+
+            url = json.url;
+            let id = json.imgPath;
+            let imgBlob = await fetch('/api/recieve_img/' + id, {
+                method: 'GET',
+                headers: {
+                    Accept: 'image/jpeg',
+                    'Content-Type': 'image/jpeg'
+                }
+            })
+
+            image.src = URL.createObjectURL(await imgBlob.blob());
+        }
+        let applicationName = document.createElement('span');
+        applicationName.textContent = imageName;
+        applicationContainer.append(image);
+        applicationContainer.append(applicationName);
+        applicationContainer.addEventListener("click", function (e) {
+            let applicationLink = url;
+            window.open(applicationLink, '_blank');
+        })
+        appContainer.append(applicationConteainer)
+    }
+
 }
+
+main()
