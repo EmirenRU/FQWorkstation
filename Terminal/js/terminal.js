0a7bc32deb6a82cee8a6ea68ec5ab9443a89a22a
PodlesniyIvanRUDN <podlessnaja101@gmail.com>
2024-11-30 20:12:28 +0300

    Terminal UI development start

---
+++ Terminal/js/terminal.js
@@ -0,0 +1,20 @@
+var term = new window.Terminal({
+    cursorBlink: true,
+});
+term.open(document.getElementById('terminal'));
+
+const socket = new WebSocket("ws://localhost:6060");
+
+let send = document.getElementById('send');
+
+send.addEventListener('submit', (e) => {
+    e.preventDefault();
+    socket.send(command + '\n');
+
+    socket.onmessage = (event) => {
+        term.write(event.data);
+}
+
+})
+
+
