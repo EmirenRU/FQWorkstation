f5d5b0d9534be59570a102db71682bc24a4c0ab4
PodlesniyIvanRUDN <podlessnaja101@gmail.com>
2025-03-21 20:09:14 +0300

    frontend changes

--- frontend/src/Functions/ProtocolDocs/SendFile.tsx
+++ frontend/src/Functions/ProtocolDocs/SendFile.tsx
@@ -60,7 +60,7 @@ export const SendFile = () => {
 
     useEffect(() => {
         // Update the ready state based on the presence of file or template
-        setReady(file !== null || template !== null);
+        setReady(file !== null && template !== null);
     }, [file, template])
 
 
