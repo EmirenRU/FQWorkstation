19f56c07a3178206d0c1d2f53501c3033fa0561e
Emil Yusupov <mrjava3300@mail.ru>
2025-04-18 19:34:00 +0300

    Update docker-start.ps1

--- docker/docker-start.ps1
+++ docker/docker-start.ps1
@@ -1,2 +1,6 @@
-Start-Process "docker-compose" -ArgumentList "up", "--build"
-pause
+if (-not (Get-Command docker-compose -ErrorAction SilentlyContinue)){
+    Write-Host "Docker is might not be installed..."
+    Pause
+} else{
+    Start-Process "docker-compose" -ArgumentList "up", "--build"
+}
