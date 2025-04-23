0067fc42dd46e0d3029c311d9499775b1df983bf
Emil Yusupov <mrjava3300@mail.ru>
2025-04-23 12:31:37 +0300

    feat(microservices: Emiren): corrected docker files and frontend

--- docker/docker-start.ps1
+++ docker/docker-start.ps1
@@ -2,8 +2,9 @@ if (-not (Get-Command docker-compose -ErrorAction SilentlyContinue)){
     Write-Host "Docker is might not be installed..."
     Pause
 } else{
-    set DOCKER_BUILDKIT=1
-    Start-Process "docker" -ArgumentList "build", "-t", "emiren-co/fqworkstation-root:latest", "../." -Wait
+    $env:DOCKER_BUILDKIT = "1"
+    $env:COMPOSE_BAKE = "true"
+#    Start-Process "docker" -ArgumentList "build", "-t", "emiren-co/fqworkstation-root:latest", "../." -Wait
 #    Start-Process "docker" -ArgumentList "save", "-o", "fqworkstation.tar", "emiren-co/fqworkstation-root:latest" -Wait
 #    Start-Process "docker" -ArgumentList "load", "-i", "fqworkstation.tar" -Wait
     Start-Process "docker-compose" -ArgumentList "up", "--build"
